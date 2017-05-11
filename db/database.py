f28f204b129c56ddd16c554030e409f87f57474f
r-wambui <rose.wambui@andela.com>
2017-05-11 12:50:22 +0300

    change the table structure

--- db/database.py
+++ db/database.py
@@ -6,20 +6,20 @@ Base = declarative_base()
 
 
 class Person(Base):
-    __tablename__ = 'persons'
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(50))
+    __tablename__ = 'People'
+    # id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), primary_key=True)
     last_name = Column(String)
     job_type = Column(String)
+    want_accomodation = Column(String)
 
 
 class Room(Base):
-    __tablename__ = 'rooms'
+    __tablename__ = 'Rooms'
     id = Column(Integer, primary_key=True)
     room_name = Column(String)
     room_type = Column(String)
 
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///database.db')
-    Base.metadata.create_all(bind=engine)
+engine = create_engine('sqlite:///database.db')
+Base.metadata.create_all(engine)
